10d9a1d863f4f66fe4b7c4b13ffcf2085bbfea43
Tedd James <tedd.james@student.moringaschool.com>
2025-04-08 21:12:11 +0300

    JSX Lab (CodeGrade)

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+// Assume this variable is defined in your data/user file:
+import { image } from "../data/user";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a paragraph about me. I love coding and building web applications.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
---
+++ src/data/user.js
@@ -0,0 +1,3 @@
+export const name = "TJ";
+export const city = "Nairobi";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
